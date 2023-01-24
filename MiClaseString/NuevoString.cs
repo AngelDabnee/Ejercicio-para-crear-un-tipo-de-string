391cf7723252af6d0e054bb53c7da81edc8da970
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-23 18:14:23 -0700

    Ejercicio donde creamos un tipo lengt, y cambiamos el valor, utilizando get y set
    Signed-off-by: Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>

---
+++ MiClaseString/NuevoString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiClaseString
+{
+    public class NuevoString
+    {
+        //Creamos los valores en modo privado para el encapsulamiento para solo poder acceder.
+        private string valor;
+        private int medida;
+        //Se realiza los medios publicos para poderlos leer. 
+        public int Medida
+        {   //Get, nos da el valor o medida del objeto
+            get => medida;
+        }   //No incluimos el set, ya que no queremos cambiar el valor de medida, ya que nos ayudará
+            //a medir la cadena y sacarnos lo que nos vale esa medida. 
+        public string Valor
+        {
+            get
+            {
+                return valor;
+            }
+            //Nos arroja el poder cambiar el valor del objeto. 
+            set
+            {
+                this.valor = value;
+                this.medida = this.valor.Length;
+            }
+        }
+        //Creamos los métodos para poder ser utilizados en program. 
+        public NuevoString(string valor)
+        {
+            this.valor = valor;
+            this.medida = valor.Length;
+
+
+        }
+    }
+}
---
+++ MiClaseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using MiClaseString;
+using System;
+
+namespace ClaseString // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Creamos el nuevo objeto, para poder utilizar el método creado del tipo string. 
+            NuevoString nuevoString = new NuevoString(" ");
+            Console.WriteLine("Que palabra quieres escribir");
+            nuevoString.Valor = Console.ReadLine();
+            Console.WriteLine("El nuevo valor de tu cadena es " + nuevoString.Valor);
+            Console.WriteLine("El total de caracteres de tu string es " + nuevoString.Medida);
+            Console.ReadKey();
+        }
+    }
+}
